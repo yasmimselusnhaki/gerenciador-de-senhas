3751aba5510d09a2f4bcaea345688a5e49f59c19
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2026-06-22 07:37:18 -0300

    mudar

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
+let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
@@ -13,6 +13,7 @@ function diminuiTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
@@ -20,6 +21,7 @@ function aumentaTamanho(){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 const campoSenha = document.querySelector('#campo-senha');
 
@@ -27,8 +29,11 @@ const letrasMaisculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 geraSenha();
 
 function geraSenha(){
-    let numeroAleatorio = Math.random()*letrasMaisculas.length;
-    console.log(numeroAleatorio);
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaisculas.length;
+        numeroAleatorio = Math.floor(letrasMaisculas[numeroAleatorio]);
+        senha = senha + letrasMaisculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
 }
-
-campoSenha.value = letrasMaisculas;
