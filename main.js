b390bdd8c005051b68d3ef5f36aadc42a94be43e
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2026-07-06 08:16:45 -0300

    salvar

--- main.js
+++ main.js
@@ -73,6 +73,7 @@ function classificaSenha(tamanhoAlfabeto){
     } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
     }
+
     const valorEntropia = document.querySelector('.entropia');
     valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
